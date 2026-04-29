b1d5a7db05e587d503bcdad3c96db2b17c43a1d1
FK78 <34196381+FK78@users.noreply.github.com>
2026-04-29 11:38:40 +0100

    feat: create hashPassword function in usersService

---
+++ src/server/services/usersService.js
@@ -0,0 +1,32 @@
+import { argon2, randomBytes } from "node:crypto";
+
+export function hashPassword(password) {
+  const nonce = randomBytes(16);
+
+  const parameters = {
+    message: password,
+    nonce,
+    parallelism: 4,
+    tagLength: 64,
+    memory: 65536,
+    passes: 3,
+  };
+
+  return new Promise((resolve, reject) => {
+    argon2("argon2id", parameters, (err, derivedKey) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        hash: derivedKey.toString("hex"),
+        nonce: nonce.toString("base64"),
+        parallelism: parameters.parallelism,
+        tagLength: parameters.tagLength,
+        memory: parameters.memory,
+        passes: parameters.passes,
+      });
+    });
+  });
+}

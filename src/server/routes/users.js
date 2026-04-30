68600b3721b84a630c36a5baee9ebee94de8a29e
FK78 <34196381+FK78@users.noreply.github.com>
2026-04-30 12:03:06 +0100

    fix: use validateSensitive instead of validate

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { validate, validateSensitive } from "../middleware/validate.js";
+import { validateSensitive } from "../middleware/validate.js";
 import * as usersController from "../controllers/usersController.js";
 
 const router = express.Router();
 
 router.post(
   "/login",
-  validate(["email", "password"]),
+  validateSensitive(["email", "password"]),
   usersController.getByEmail,
 );
 
